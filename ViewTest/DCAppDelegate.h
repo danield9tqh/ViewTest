5d2871e8d4c3efe21f64a0ca65f130c53481d9b8
Daniel <Daniel@wl-214-91.wpa.claremont.edu>
2014-09-10 10:55:48 -0700

    Initial Commit

---
+++ ViewTest/DCAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  DCAppDelegate.h
+//  ViewTest
+//
+//  Created by Daniel Cogan on 9/10/14.
+//  Copyright (c) 2014 Daniel Cogan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DCAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
